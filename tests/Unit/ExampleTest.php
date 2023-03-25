1e8e8cb00f0e53dcc93cf212563cb78da8d577ce
juaneescobar <juane.escobard@gmail.com>
2023-03-25 16:34:24 -0500

    unit testing

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,13 +4,20 @@ namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+require_once 'Calculator.php';
+
+class CalculatorTest extends TestCase
 {
-    /**
-     * A basic test example.
-     */
-    public function test_that_true_is_true(): void
+    public function testSum(): void
     {
-        $this->assertTrue(true);
+        $calculator = new Calculator();
+        $result = $calculator->sum(1, 2);
+        $this->assertEquals(3, $result);
+
+        $result = $calculator->sum(-1, 1);
+        $this->assertEquals(0, $result);
+
+        $result = $calculator->sum(0.1, 0.2);
+        $this->assertEquals(0.3, $result, '', 0.0001);
     }
 }
